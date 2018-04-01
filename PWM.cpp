1d2cd5b21c2766cebbfec02bcbfed1d4f23b1689
Devin Gund <devin@dgund.com>
2018-04-01 01:03:08 -0400

    Implement PWM class

---
+++ PWM.cpp
@@ -0,0 +1,102 @@
+/*
+    Copyright 2018 Devin Gund (https://dgund.com)
+    https://github.com/dgund/BBBKit/
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "PWM.h"
+
+#include "Readwrite.h"
+
+#define PWM_SYSFS_PATH "/sys/devices/ocp.3/"
+#define PWM_SYSFS_DUTY "duty"
+#define PWM_SYSFS_PERIOD "period"
+#define PWM_SYSFS_POLARITY "polarity"
+#define PWM_SYSFS_RUN "run"
+
+#define PERCENT_MIN 0.0
+#define PERCENT_MAX 100.0
+#define NANO 1000000000.0
+
+namespace bbbkit {
+
+PWM::PWM(PWM::PIN pin, PWM::VALUE activeState) {
+    this->name = this->nameMap[pin];
+    this->path = PWM_SYSFS_PATH + this->name + "/";
+    this->setActiveState(activeState);
+}
+
+PWM::~PWM() {}
+
+
+PWM::VALUE PWM::getActiveState() {
+    if (std::stoi(read(this->path, PWM_SYSFS_POLARITY)) == PWM::VALUE::LOW) return PWM::VALUE::LOW;
+    else return PWM::VALUE::HIGH;
+}
+
+int PWM::setActiveState(PWM::VALUE activeState) {
+    return write(this->path, PWM_SYSFS_POLARITY, activeState);
+}
+
+unsigned int PWM::getPeriod() {
+    return std::stoi(read(this->path, PWM_SYSFS_PERIOD));
+}
+
+int PWM::setPeriod(unsigned int periodNS) {
+    return write(this->path, PWM_SYSFS_PERIOD, periodNS);
+}
+
+float PWM::getFrequency() {
+    unsigned int periodNS = this->getPeriod();
+    float periodS = (float)periodNS / NANO;
+    float frequencyHZ = 1.0 / periodS;
+    return frequencyHZ;
+}
+
+int PWM::setFrequency(float frequencyHZ) {
+    float periodS = 1.0 / frequencyHZ;
+    unsigned int periodNS = (unsigned int)(periodS * NANO);
+    return this->setPeriod(periodNS);
+}
+
+unsigned int PWM::getDutyCycle() {
+    return std::stoi(read(this->path, PWM_SYSFS_DUTY));
+}
+
+float PWM::getDutyCycleAsPercent() {
+    return PERCENT_MAX * (float)this->getDutyCycle() / (float)this->getPeriod();
+}
+
+int PWM::setDutyCycle(unsigned int dutyCycleNS) {
+    return write(this->path, PWM_SYSFS_DUTY, dutyCycleNS);
+}
+
+int PWM::setDutyCycleAsPercent(float dutyCyclePercent) {
+    if (PERCENT_MIN > dutyCyclePercent || dutyCyclePercent > PERCENT_MAX) return -1;
+    return this->setDutyCycle((float)this->getPeriod * (dutyCyclePercent / PERCENT_MAX));
+}
+
+int PWM::start() {
+    return write(this->path, PWM_SYSFS_RUN, PWM::VALUE::HIGH);
+}
+
+bool PWM::isRunning() {
+    return std::stoi(read(this->path, PWM_SYSFS_RUN)) == PWM::VALUE::HIGH;
+}
+
+int PWM::stop() {
+    return write(this->path, PWM_SYSFS_RUN, PWM::VALUE::LOW);
+}
+
+} /* namespace bbbkit */
---
+++ PWM.h
@@ -0,0 +1,89 @@
+/*
+    Copyright 2018 Devin Gund (https://dgund.com)
+    https://github.com/dgund/BBBKit/
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include <string>
+
+namespace bbbkit {
+
+class PWM {
+public:
+    enum PIN { P8_13    = 0,
+               P8_19    = 1,
+               P9_14    = 2,
+               P9_16    = 3,
+               P9_21    = 4,
+               P9_22    = 5,
+               P9_42    = 6,
+               EHRPWM2B = P8_13,
+               EHRPWM2A = P8_19,
+               EHRPWM1A = P9_14,
+               EHRPWM1B = P9_16,
+               EHRPWM0B = P9_21,
+               EHRPWM0A = P9_22,
+               ECAP0    = P9_42,
+             };
+
+    enum VALUE{ LOW=0, HIGH=1 };
+
+private:
+    const std::string nameMap[7] = { "P8_13",
+                                     "P8_19",
+                                     "P9_14",
+                                     "P9_16",
+                                     "P9_21",
+                                     "P9_22",
+                                     "P9_42",
+                                   };
+
+    std::string path;
+    std::string name;
+
+public:
+    PWM(PWM::PIN pin, PWM::VALUE activeState=PWM::VALUE::HIGH);
+    virtual ~PWM();
+
+    // Getters and setters
+
+    virtual PWM::PIN getPin() { return pin; }
+    virtual std::string getPath() { return path; }
+    virtual std::string getName() { return name; }
+
+    virtual PWM::VALUE getActiveState();
+    virtual int setActiveState(PWM::VALUE activeState);
+
+    virtual unsigned int getPeriod();
+    virtual int setPeriod(unsigned int periodNS);
+
+    virtual float getFrequency();
+    virtual int setFrequency(float frequencyHZ);
+    
+    virtual unsigned int getDutyCycle();
+    virtual float getDutyCycleAsPercent();
+    virtual int setDutyCycle(unsigned int dutyCycleNS);
+    virtual int setDutyCycleAsPercent(float dutyCyclePercent);
+
+    virtual int start();
+    virtual bool isRunning();
+    virtual int stop();
+};
+
+} /* namespace bbbkit */
+
+#endif /* PWM_H_ */
