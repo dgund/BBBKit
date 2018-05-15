108e39e1d047e5e2dc7a6b9116dd7381e25fecad
Devin Gund <devin@dgund.com>
2018-05-15 19:14:00 -0400

    Add I2C class

---
+++ I2C.cpp
@@ -0,0 +1,132 @@
+/*
+    Copyright 2018 Devin Gund (https://dgund.com)
+    https://github.com/dgund/BBBKit/
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "I2C.h"
+
+#include <iostream>
+#include <sstream>
+
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+
+#define I2C_SYSFS_PATH "/dev/i2c-"
+
+#define I2C_FILE_NULL -1
+
+namespace bbbkit {
+
+I2C::I2C(I2C::BUS bus, unsigned int deviceAddress) {
+    this->bus = bus;
+
+    // Format path for the BeagleBone
+    std::stringstream pathStream;
+    pathStream << I2C_SYSFS_PATH << static_cast<int>(this->bus);
+    this->path = pathStream.str();
+
+    this->deviceAddress = deviceAddress;
+    this->file = I2C_FILE_NULL;
+}
+
+I2C::~I2C() {
+    if (this->isOpen()) {
+        this->close();
+    }
+}
+
+int I2C::open() {
+    // Open I2C bus
+    if ((this->file = std::open(this->path, O_RDWR)) < 0) {
+        std::cout << "I2C: Error opening bus." << std::endl;
+        return -1;
+    }
+
+    // Connect to I2C device
+    if (ioctl(this->file, I2C_SLAVE, this->deviceAddress) < 0) {
+        std::cout << "I2C: Error connecting to device." << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+bool I2C::isOpen() {
+    return (this->file != I2C_FILE_NULL);
+}
+
+int I2C::write(unsigned char value) {
+    // Write value to device
+    unsigned char buffer[1];
+    buffer[0] = value;
+    if (std::write(this->file, buffer, 1) != 1) {
+        std::cout << "I2C: Error writing to device." << std::endl;
+        return -1;
+    }
+    return 0;
+}
+
+int I2C::writeRegister(unsigned int address, unsigned char value) {
+    // Write register address and value to device
+    unsigned char buffer[2];
+    buffer[0] = address;
+    buffer[1] = value;
+    if (std::write(this->file, buffer, 2) != 2) {
+        std::cout << "I2C: Error writing to device register." << std::endl;
+        return -1;
+    }
+}
+
+unsigned char I2C::readRegister(unsigned int address) {
+    // Request data from register address
+    this->write(address);
+
+    // Read response
+    unsigned char buffer[1];
+    if (std::read(this->file, buffer, 1) != 1) {
+        std::cout << "I2C: Error reading from device register." << std::endl;
+        return -1;
+    }
+
+    // Return data
+    return buffer[0];
+}
+
+unsigned char *I2C::readRegisters(unsigned int startAddress, unsigned int count) {
+    // Request data from registers starting at startAddress
+    this->write(startAddress);
+
+    // Read response
+    unsigned char *buffer[count];
+    if (std::read(this->file, buffer, count) != (int)count) {
+        std::cout << "I2C: Error reading from device registers." << std::endl;
+        return NULL;
+    }
+
+    // Return data (must be freed externally)
+    return buffer;
+}
+
+void I2C::close() {
+    if (this->isOpen()) {
+        std::cout << "I2C: Bus already closed." << std::endl;
+    } else {
+        std::close(this->file);
+        this->file = I2C_FILE_NULL;
+    }
+}
+
+} /* namespace bbbkit */
---
+++ I2C.h
@@ -0,0 +1,64 @@
+/*
+    Copyright 2018 Devin Gund (https://dgund.com)
+    https://github.com/dgund/BBBKit/
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef I2C_H
+#define I2C_H
+
+namespace bbbkit {
+
+class I2C {
+public:
+    enum BUS {
+        I2C_1 = 1,
+        I2C_2 = 2,
+    };
+
+private:
+    I2C::BUS bus;
+    std::string path;
+    unsigned int deviceAddress;
+    int file;
+
+public:
+    I2C(I2C::BUS bus, unsigned int deviceAddress);
+    virtual ~I2CDevice();
+
+    // Open I2C bus to device
+    virtual int open();
+
+    // Check if I2C bus is open
+    virtual bool isOpen();
+
+    // Write value to I2C device
+    virtual int write(unsigned char value);
+
+    // Write value to I2C device register
+    virtual int writeRegister(unsigned int address, unsigned char value);
+
+    // Read value from I2C device register
+    virtual unsigned char readRegister(unsigned int address);
+
+    // Read value from multiple I2C device registers (in one read operation)
+    virtual unsigned char *readRegisters(unsigned int startAddress=0x0, unsigned int count);
+
+    // Close I2C bus to device
+    virtual void close();
+};
+
+} /* namespace bbbkit */
+
+#endif /* I2C_H */
